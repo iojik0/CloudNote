706ce5bc1fe05b4dcd8b4c4f1e876ef1f37677b4
iojik0 <xmanlive00111@gmail.com>
2026-04-08 00:01:36 +0300

    Initial commit: CloudNoye JavaFX application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.cloudnote.cloudnote {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.cloudnote to javafx.fxml;
+    exports com.cloudnote;
+    exports com.cloudnote.controller;
+    opens com.cloudnote.controller to javafx.fxml;
+}
